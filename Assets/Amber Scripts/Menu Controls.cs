b4f10ded773784ea7a2e4bca323e9e65a3ded5c2
YaelE25 <157845181+YaelE25@users.noreply.github.com>
2025-03-03 18:07:34 -0600

    Revert "CreditsImplemented"
    This reverts commit 593a92a61c0f7fc30ce33178b58dff15734f4c81.

--- Assets/Amber Scripts/Menu Controls.cs
+++ Assets/Amber Scripts/Menu Controls.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
-    [SerializeField] private GameObject MainMenuObjects;
-    [SerializeField] private GameObject CreditsObject;
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -19,23 +16,5 @@ public class MenuControls : MonoBehaviour
     public void Menu()
     {
         SceneManager.LoadScene("Main Menu");
-    }
-
-    public void ShowCredits()
-    {
-        MainMenuObjects.SetActive(false);
-        CreditsObject.SetActive(true);
-    }
-
-    public void HideCredits()
-    {
-        MainMenuObjects.SetActive(true);
-        CreditsObject.SetActive(false);
-    }
-
-    public void Back()
-    {
-        MainMenuObjects.SetActive(true);
-        CreditsObject.SetActive(false);
     }
 }
