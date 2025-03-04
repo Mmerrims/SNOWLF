51573a96cb04cb4329b060dd37a7af0613dfb2d4
YaelE25 <157845181+YaelE25@users.noreply.github.com>
2025-03-03 18:19:28 -0600

    CreditsImplemented

--- Assets/Amber Scripts/Menu Controls.cs
+++ Assets/Amber Scripts/Menu Controls.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class MenuControls : MonoBehaviour
 {
+    [SerializeField] private GameObject MainMenuObjects;
+    [SerializeField] private GameObject CreditsObject;
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -16,5 +19,23 @@ public class MenuControls : MonoBehaviour
     public void Menu()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Back()
+    {
+        MainMenuObjects.SetActive(true);
+        CreditsObject.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        MainMenuObjects.SetActive(false);
+        CreditsObject.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        MainMenuObjects.SetActive(true);
+        CreditsObject.SetActive(false);
     }
 }
