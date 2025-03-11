4770d89144579f6281fd9155797db3566f3d7164
YaelE25 <157845181+YaelE25@users.noreply.github.com>
2025-03-11 13:41:20 -0500

    FireImplementation

--- Assets/Yael Scripts/SetScale.cs
+++ Assets/Yael Scripts/SetScale.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class SetScale : MonoBehaviour
 {
@@ -13,15 +16,14 @@ public class SetScale : MonoBehaviour
     [SerializeField,Tooltip("Minimum speed that the ball must goes before it scales")] private float SnolfSpeed = .1f;
     [SerializeField] private float maxScaleSpeed = 8f;
     [SerializeField] private bool useSpeed;
+    [SerializeField] private ScaleChangerType scaleType;
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Rigidbody rb))
         {
             rigidbodies.Add(rb);
-        }
-        
+        }   
     }
-
     private void OnTriggerExit(Collider other)
     {
         if (other.TryGetComponent(out Rigidbody rb))
@@ -41,7 +43,7 @@ public class SetScale : MonoBehaviour
         {
             foreach (var rb in rigidbodies)
             {
-                if( rb.velocity.magnitude < SnolfSpeed)
+                if(rb.velocity.magnitude < SnolfSpeed)
                 {
                     continue;
                 }
@@ -52,9 +54,23 @@ public class SetScale : MonoBehaviour
                 snolfScale = new Vector3(sizeController, sizeController, sizeController);
                 rb.transform.localScale = snolfScale;
                 print("after, " + snolfScale);
+                if(scaleType == ScaleChangerType.FIRE && sizeController <= minScale)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
             }
 
             yield return new WaitForSeconds(scaleDelay);
         }
     }
+
+
+}
+
+public enum ScaleChangerType
+{
+    SNOW,
+    SALT,
+    FIRE,
+    LAVA
 }
