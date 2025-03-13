47e0e92a68ccca4ea4725f91e65eacf37fe5cc8f
YaelE25 <157845181+YaelE25@users.noreply.github.com>
2025-03-13 14:13:57 -0500

    ScriptDescriptions

--- Assets/Yael Scripts/SetScale.cs
+++ Assets/Yael Scripts/SetScale.cs
@@ -1,3 +1,11 @@
+/*******************************************************************
+// File Name :         SetScale.cs
+// Author :            Yael Martoral
+// Creation Date :     3/11/2025
+//
+// Brief Description : It controls actions that player can take outside
+// of the main control scheme of controlling the snolf ball
+/********************************************************************/
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,15 +16,17 @@ using UnityEngine.UIElements;
 
 public class SetScale : MonoBehaviour
 {
-    [SerializeField] private float scale = 1f;
-    [SerializeField] private float maxScale = 5f;
-    [SerializeField] private float minScale = 1f;
+    [SerializeField,Tooltip("Scale that the ball starts when the scene is loaded")] private float scale = 1f;
+    [SerializeField,Tooltip("The maximum that the ball can grow too")] private float maxScale = 5f;
+    [SerializeField,Tooltip("The minimum that the ball can shrink too")] private float minScale = 1f;
     private List<Rigidbody> rigidbodies = new ();
-    [SerializeField] private float scaleDelay = .1f;
+    [SerializeField,Tooltip("Time before the snolf ball can scale again")] private float scaleDelay = .1f;
     [SerializeField,Tooltip("Minimum speed that the ball must goes before it scales")] private float SnolfSpeed = .1f;
-    [SerializeField] private float maxScaleSpeed = 8f;
-    [SerializeField] private bool useSpeed;
-    [SerializeField] private ScaleChangerType scaleType;
+    [SerializeField,Tooltip("The scale of the speed increases once the snolf ball increase its speed")] private float maxScaleSpeed = 8f;
+    [SerializeField,Tooltip("Determine if an object uses the speed variable to change the snolf ball")] private bool useSpeed;
+    [SerializeField,Tooltip("It decided the type of area of the object in the inspector")] private ScaleChangerType scaleType;
+
+    //Once a Rigidbody enters a trigger, it changes the respective Rigidbody
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Rigidbody rb))
@@ -24,6 +34,8 @@ public class SetScale : MonoBehaviour
             rigidbodies.Add(rb);
         }   
     }
+
+    //Once the Rigidbody exists the trigger, it stops altering the Rigidbody
     private void OnTriggerExit(Collider other)
     {
         if (other.TryGetComponent(out Rigidbody rb))
@@ -32,11 +44,13 @@ public class SetScale : MonoBehaviour
         }
     }
 
+    //It enables the stated courotines
     private void OnEnable()
     {
         StartCoroutine(ScaleOverTime());
     }
 
+    //A coroutine that controls the scale of the ball depending on what type of area the snolf area is in and alters it's scale acordingly
     IEnumerator ScaleOverTime()
     {
         while (gameObject.activeSelf)
