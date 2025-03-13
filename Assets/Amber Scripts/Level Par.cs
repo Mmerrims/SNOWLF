84687839ff0fe721a557a9f068388981414b32c9
QMerrims <mmerrims@mail.bradley.edu>
2025-03-13 14:05:30 -0500

    Revert "Merge branch 'main' of https://github.com/Mmerrims/SNOWLF"
    This reverts commit ba208521cd8996c553c950582adf6a7320eeb609, reversing
    changes made to 58c4073f861fdbf65c42697be20a9f3105ae0b27.

--- Assets/Amber Scripts/Level Par.cs
+++ Assets/Amber Scripts/Level Par.cs
@@ -1,10 +1,3 @@
-/*****************************************************************************
-// File Name :         Line Force.cs
-// Author :            Amber C. Cardamone
-// Creation Date :     March 13th, 2025
-//
-// Brief Description : Gives the level's par.
-*****************************************************************************/
 using UnityEngine;
 
 public class LevelPar : MonoBehaviour
--- Assets/Amber Scripts/Par Manager.cs
+++ Assets/Amber Scripts/Par Manager.cs
@@ -1,16 +1,10 @@
-/*****************************************************************************
-// File Name :         Line Force.cs
-// Author :            Amber C. Cardamone
-// Creation Date :     March 13th, 2025
-//
-// Brief Description : Makes the level par function, showing the level's par at the end and if you succeeded or not.
-*****************************************************************************/
 using TMPro;
 using UnityEngine;
 
 public class ParManager : MonoBehaviour
 {
     private static ParManager instance;
+    private float levelPar;
     private float playerPar;
     [SerializeField] private LevelPar _levelPar;
     [SerializeField] private GameObject _levelParText;
@@ -18,9 +12,6 @@ public class ParManager : MonoBehaviour
     [SerializeField] private TMP_Text _levelParTextbox;
     [SerializeField] private TMP_Text _levelParScoreNameText;
 
-    /// <summary>
-    /// Makes it so this object/script can travel across scenes, keeping its information.
-    /// </summary>
     private void Awake()
     {
         // Checks if there is no instance of the GameManager in the scene
@@ -38,62 +29,56 @@ public class ParManager : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Checks the current par of the scene, displays it to the player at the end of the level
-    /// </summary>
     private void FixedUpdate()
     {
-        // Finds the current object in the scene with the Level Par script
         _levelPar = FindObjectOfType<LevelPar>();
-        // Finds the text object showing the player's score, with the object having the name "Player Score Text"
+
         _levelParText = GameObject.Find("Player Score Text");
-        // Finds the text object showing the level's score, with the object having the name "Score Name Text"
         _levelParScoreName = GameObject.Find("Score Name Text");
-        // Checks if the par text is found
         if (_levelParText != null)
         {
-            // Finds the text component on the level par text object
             _levelParTextbox = _levelParText.GetComponent<TMP_Text>();
-            // Changes the score displayed to the player's ending score
+
             _levelParTextbox.text = ("Score: " + playerPar);
         }
 
-        // Checks if the score text was found
         if (_levelParScoreName != null)
         {
-            // Makes the level's par text the same as the found level's par
             _levelParScoreNameText = _levelParScoreName.GetComponent<TMP_Text>();
         }
 
-        // Checks if the level's par was found and the level's score name text was found
-        if (_levelPar != null && _levelParScoreName != null)
+
+
+        if (_levelPar == null)
         {
-            // Checks if the level's par is greater then the player's par
-            if (_levelPar.LevelsPar > playerPar)
+            print("No level par found");
+        }
+
+        if (_levelPar != null)
+        {
+            if (_levelPar.LevelsPar > playerPar && _levelParScoreName != null)
             {
-                // Makes the text display that the player got under par
+
                 _levelParScoreNameText.text = ("Under Par!");
             }
-            // Checks if the level's par is equal to the player's score
-            else if (_levelPar.LevelsPar == playerPar)
+            else if (_levelPar.LevelsPar == playerPar && _levelParScoreName != null)
             {
-                // Makes the text display that the player got on par
                 _levelParScoreNameText.text = ("On Par!");
             }
-            // Automatically goes to this if the other two fail, meaning the player got above par
             else
             {
-                // Makes the text display that the player got above par
-                _levelParScoreNameText.text = ("Above Par.");
+                if (_levelParScoreName != null)
+                {
+                    _levelParScoreNameText.text = ("Above Par.");
+                }
             }
         }
     }
 
-    /// <summary>
-    /// Adds a point to the player's par.
-    /// </summary>
     public void AddHit()
     {
         playerPar += 1;
+        
+        
     }
 }
